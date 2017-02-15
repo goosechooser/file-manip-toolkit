f902265097046951ecce83e1796f45229725f273
Marshall <xtremebass2k@gmail.com>
2017-02-15 02:56:24 -0800

    changed to reflect new organization scheme

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'unfman=file_manip_toolkit.cli:unfman_main',
+            'unfman=file_manip_toolkit.unfman.cli:unfman_main',
             'eswap=file_manip_toolkit.eswap.cli:main'],
         }
     )
