9527e5621e4cb1a3d9822409d44d9a655664f11c
Marshall <xtremebass2k@gmail.com>
2017-02-15 02:54:09 -0800

    parametrizing tests

--- tests/eswap_test.py
+++ tests/eswap_test.py
@@ -8,55 +8,24 @@ TESTDATA2 = bytearray.fromhex("0A 0B 0C 0D 0A 0B 0C 0D")
 TESTDATA3 = bytearray.fromhex("0A 0B 0C 0D 0A 0B 0C 0D 0A 0B 0C 0D 0A 0B 0C 0D")
 TESTFILE = 'testdir\\vm3.15'
 
-def test_swap_h():
-    result = eswap.swap(TESTDATA, 'h')
-    assert(result) == bytearray.fromhex("0B 0A 0D 0C")
-    result2 = eswap.swap(result, 'h')
-    assert(result2) == TESTDATA
-
-def test_swap_H():
-    result = eswap.swap(TESTDATA, 'H')
-    assert(result) == bytearray.fromhex("0B 0A 0D 0C")
-    result2 = eswap.swap(result, 'H')
-    assert(result2) == TESTDATA
-
-# Check this out - struct.error: bad char in struct format
-# def test_swap_e():
-#     result = eswap.swap(TESTDATA, 'e')
-#     assert(result) == bytearray.fromhex("0B 0A 0D 0C")
-#     result2 = eswap.swap(result, 'e')
-#     assert(result2) == TESTDATA
-
-def test_swap_i():
-    result = eswap.swap(TESTDATA, 'i')
-    assert(result) == bytearray.fromhex("0D 0C 0B 0A")
-    result2 = eswap.swap(result, 'i')
-    assert(result2) == TESTDATA
-
-def test_swap_I():
-    result = eswap.swap(TESTDATA, 'I')
-    assert(result) == bytearray.fromhex("0D 0C 0B 0A")
-    result2 = eswap.swap(result, 'I')
-    assert(result2) == TESTDATA
-
-def test_swap_q():
-    result = eswap.swap(TESTDATA2, 'q')
-    assert(result) == bytearray.fromhex("0D 0C 0B 0A 0D 0C 0B 0A")
-    result2 = eswap.swap(result, 'q')
-    assert(result2) == TESTDATA2
-
-def test_swap_Q():
-    result = eswap.swap(TESTDATA2, 'Q')
-    assert(result) == bytearray.fromhex("0D 0C 0B 0A 0D 0C 0B 0A")
-    result2 = eswap.swap(result, 'Q')
-    assert(result2) == TESTDATA2
-
-def test_swap_exception():
+@pytest.mark.parametrize("test_input, fmt, expected", [
+    (TESTDATA, 'h', bytearray.fromhex("0B 0A 0D 0C")),
+    (TESTDATA, 'H', bytearray.fromhex("0B 0A 0D 0C")),
+    (TESTDATA, 'i', bytearray.fromhex("0D 0C 0B 0A")),
+    (TESTDATA, 'I', bytearray.fromhex("0D 0C 0B 0A")),
+    (TESTDATA2, 'q', bytearray.fromhex("0D 0C 0B 0A 0D 0C 0B 0A")),
+    (TESTDATA2, 'Q', bytearray.fromhex("0D 0C 0B 0A 0D 0C 0B 0A")),
+])
+def test_swap(test_input, fmt, expected):
+    assert eswap.swap(test_input, fmt) == expected
+    
+@pytest.mark.parametrize("fmt", [
+    ('Q'),
+    ('e')
+])
+def test_swap_exception(fmt):
     with pytest.raises(struct.error):
-        eswap.swap(TESTDATA, 'Q')
-
-    with pytest.raises(struct.error):
-        eswap.swap(TESTDATA, 'e')
+        eswap.swap(TESTDATA, fmt)
 
 # format_filename(filename, savepath, suffix)
 def test_format_filename_default():
--- tests/file_manip_test.py
+++ tests/file_manip_test.py
@@ -1,6 +1,6 @@
 import struct
 import pytest
-from file_manip_toolkit import file_manip
+from file_manip_toolkit.unfman import file_manip
 
 TESTDATA = bytearray.fromhex('0A 0B 0C 0D A0 B0 C0 D0')
 TESTDATA2 = bytearray.fromhex('FA FB FC FD AF BF CF DF')
@@ -18,20 +18,26 @@ def test_deinterleave():
 
     #more tests related nbytes/nsplit?
 
-def test_deinterleave_exception():
+#todo - figure out what exception the following cases are raising
+#(TESTDATA, 8, 2),
+#(TESTDATA, 1, 1),
+@pytest.mark.parametrize('test_input, nbytes, nsplit', [
+    (TESTDATA, 3, 2),
+    (TESTDATA, 99, 2),
+    #(TESTDATA, 8, 2),
+    #(TESTDATA, 1, 1),
+])
+def test_deinterleave_exception(test_input, nbytes, nsplit):
     with pytest.raises(struct.error):
-        file_manip.deinterleave(TESTDATA, 3, 2)
-        file_manip.deinterleave(TESTDATA, 99, 2)
-        file_manip.deinterleave(TESTDATA, 99, 2)
-        file_manip.deinterleave(TESTDATA, 8, 1)
-        file_manip.deinterleave(TESTDATA, 1, 1)
+        file_manip.deinterleave(test_input, nbytes, nsplit)
 
-def test_interleave():
-    result = file_manip.interleave([TESTDATA, TESTDATA2], 2)
-    assert result == bytearray.fromhex('0A 0B FA FB 0C 0D FC FD A0 B0 AF BF C0 D0 CF DF')
-
-    result = file_manip.interleave([TESTDATA, TESTDATA2], 4)
-    assert result == bytearray.fromhex('0A 0B 0C 0D FA FB FC FD A0 B0 C0 D0 AF BF CF DF')
+@pytest.mark.parametrize('test_input, nsplit, expected', [
+    ([TESTDATA, TESTDATA2], 2, '0A 0B FA FB 0C 0D FC FD A0 B0 AF BF C0 D0 CF DF'),
+    ([TESTDATA, TESTDATA2], 4, '0A 0B 0C 0D FA FB FC FD A0 B0 C0 D0 AF BF CF DF'),
+])
+def test_interleave(test_input, nsplit, expected):
+    result = file_manip.interleave(test_input, nsplit)
+    assert result == bytearray.fromhex(expected)
 
 def test_interleave_exception():
     with pytest.raises(struct.error):
