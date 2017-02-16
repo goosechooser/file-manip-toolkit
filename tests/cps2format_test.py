cc6450ca7ebafe701093feb2729f2603e929fd04
Marshall <xtremebass2k@gmail.com>
2017-02-16 00:12:33 -0800

    tests work, but ugly

--- tests/cps2format_test.py
+++ tests/cps2format_test.py
@@ -1,53 +1,60 @@
 import os
+import os.path
 import filecmp
 import pytest
 from file_manip_toolkit.unfman import CPS2Format
 
 TESTDIR = 'tests\\testdir'
-FILES = ['vm3.13', 'vm3.15', 'vm3.17', 'vm3.19']
-PREFIX = 'known_good'
-GOODFILES = ['\\'.join([PREFIX, fname]) for fname in FILES]
-DEINTERLEAVE_FILE = 'tests\\testdir\\known_good\\cps2_test.13.15.17.19.combined'
+DEINTERLEAVE_FILE = 'cps2_test.13.15.17.19.combined'
+INTERLEAVE_FILES = ['cps2_test.13', 'cps2_test.15', 'cps2_test.17', 'cps2_test.19']
+CUSTOM_FILES = ['custom.13', 'custom.15', 'custom.17', 'custom.19']
 
-NBYTES = [2, 4]
-NSPLITS = [2, 4]
-OUTPUTS = ['', 'testdir\\', 'testdir\\custom']
+# tests work for the most part
+# but some thought should be given to refactoring them
+# maybe
 
-@pytest.fixture
-def cps2_inter(output):
-    files = ['\\'.join([TESTDIR, PREFIX, fname]) for fname in FILES]
-    return CPS2Format.new(files, None, output, False)
-
-@pytest.fixture(params=NBYTES)
+@pytest.fixture(params=[2, 4])
 def numbytes(request):
     return request.param
 
-@pytest.fixture(params=NSPLITS)
+@pytest.fixture(params=[2, 4])
 def nsplits(request):
     return request.param
 
-@pytest.fixture(params=OUTPUTS)
+@pytest.fixture(params=['testdir\\', 'custom'])
 def outputs(request):
     return request.param
 
-# @pytest.fixture(params=[FILES, [DEINTERLEAVE_FILE]])
-# def filepaths(request):
-#     return request.param
-
-@pytest.fixture
-def temp_dirs(tmpdir_factory, outputs):
-    return [str(tmpdir_factory.mktemp(name)) for name in outputs]
-
-# @pytest.mark.parametrize("test_input, expected", [
-#     (['eswap_placeholder', 'test', 'filetemp', '-o', 'here'], -1),
-#     (['eswap_placeholder', 'testdir\\vm3.13', 'h', '-o', 'there'], 0),
-# ])
-
-@pytest.mark.parametrize('filepaths, expected', [
-    (FILES, 1),
-    ([DEINTERLEAVE_FILE], 4),
+@pytest.mark.parametrize('filepaths, outputs, expected', [
+    (INTERLEAVE_FILES, '', (1, [DEINTERLEAVE_FILE])),
+    (INTERLEAVE_FILES, 'testdir/', (1, [''.join(['testdir/', DEINTERLEAVE_FILE])])),
+    (INTERLEAVE_FILES, 'testdir/custom', (1, ['testdir/custom.combined'])),
+    ([DEINTERLEAVE_FILE], '', (4, INTERLEAVE_FILES)),
+    ([DEINTERLEAVE_FILE], 'testdir/',
+     (4, [''.join(['testdir/', name]) for name in INTERLEAVE_FILES])),
+    ([DEINTERLEAVE_FILE], 'testdir/custom',
+     (4, [''.join(['testdir/', name]) for name in CUSTOM_FILES])),
 ])
-def test_format_filenames(filepaths, expected, outputs):
+def test_format_filenames(filepaths, outputs, expected):
     cps2 = CPS2Format.new(filepaths, None, outputs, False)
     results = cps2.format_filenames()
-    assert len(results) == expected
+    numfiles, filenames = expected
+    assert len(results) == numfiles
+    assert results == filenames
+
+# deinterleave runs slower during testing - find out why?
+# DOESNT WORK WITH empty case?'' not sure how to test
+@pytest.mark.parametrize('test_data, expected', [
+    ([''.join(['tests/testdir/', DEINTERLEAVE_FILE])], 4),
+    ([''.join(['tests/testdir/', name]) for name in INTERLEAVE_FILES], 1),
+])
+def test_run(test_data, expected, outputs, tmpdir):
+    fns = tmpdir.mkdir(outputs)
+
+    cps2 = CPS2Format.new(test_data, None, str(fns), False)
+    cps2.run()
+
+    assert len(tmpdir.join(outputs).listdir()) == expected
+
+    for f in tmpdir.join(outputs).listdir():
+        assert filecmp.cmp(f.strpath, os.path.join(TESTDIR, f.basename))
