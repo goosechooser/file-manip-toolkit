7a6999b14dcf55cddeb155de4c5c437fef78fef7
Marshall <xtremebass2k@gmail.com>
2017-02-23 04:52:01 -0800

    refactored file/savepath logic into 2 methods

--- file_manip_toolkit/unfman/CPS2Format.py
+++ file_manip_toolkit/unfman/CPS2Format.py
@@ -1,64 +1,18 @@
 import os
 from struct import Struct
-from file_manip_toolkit.unfman import file_manip
-from file_manip_toolkit.unfman import FileFormat
+from file_manip_toolkit.unfman.FileFormat import FileFormat
 
-# Can cut 6-7 seconds of run time if use old cps2_manip code
-# which is acceptable since this IS the cps2 specific formatter
-# do that later when u get sleep
+# Current times for deinterleave/interleave are ~10s each
+# Implement threading for a speedup?
 
-def deinterleave(data, num_bytes):
-    """Deinterleaves a bytearray.
-
-    Returns two bytearrays.
-    """
-    evens = []
-    odds = []
-    deinterleave_s = Struct('c' * num_bytes)
-    deinterleave_iter = deinterleave_s.iter_unpack(data)
-
-    for i in deinterleave_iter:
-        evens.extend([*i])
-        odds.extend([*next(deinterleave_iter)])
-
-    print('len of evens is:', len(evens), 'len of odds is:', len(odds))
-    return b''.join(evens), b''.join(odds)
-
-def interleave(file1, file2, num_bytes):
-    """Interleaves two bytearray buffers together.
-
-    Returns a bytearray.
-    """
-    interleaved = []
-    interleave_s = Struct('c' * num_bytes)
-    file1_iter = interleave_s.iter_unpack(file1)
-    file2_iter = interleave_s.iter_unpack(file2)
-
-    for i in file1_iter:
-        file2_next = next(file2_iter)
-        interleave_temp = [*i, *file2_next]
-        interleaved.extend(interleave_temp)
-
-    return  b''.join(interleaved)
-
-class CPS2Format(FileFormat.FileFormat):
-    @staticmethod
-    def _reshape(list_):
-        zipdata = zip(*list_)
-
-        reshape = []
-        for z in zipdata:
-            reshape.extend([*z])
-
-        return [reshape[i:i+2] for i in range(0, len(reshape), 2)]
-
+class CPS2Format(FileFormat):
     def run(self):
+        savepaths = self.format_savepaths()
+
         if len(self._filepaths) == 1:
             final = self.deinterleave_file()
         else:
             final = self.interleave_files()
-
-        savepaths = self.format_filenames()
 
         self.save(savepaths, final)
 
@@ -66,16 +20,10 @@ class CPS2Format(FileFormat.FileFormat):
         """Interleaves a set of 4 cps2 graphics files."""
 
         self.verboseprint('Opening files')
-        data = [file_manip.open_file(fp) for fp in self._filepaths]
+        data = [self.open_file(fp) for fp in self._filepaths]
 
         self.verboseprint('Splitting files')
         split_data = [deinterleave(fsplit, 2) for fsplit in data]
-        # initial split gives us something like:
-        # [['13e', '13o'], ['15e', '15o'], ['17e', '17o'], ['19e', '19o']]
-        # want it shaped like this:
-        # [['13e', '15e'], ['17e', '19e'], ['13o', '15o'], ['17o', '19o']]
-
-        # reshaped = self._reshape(split_data)
 
         self.verboseprint('First pass - interleaving every 2 bytes')
         interleaved = []
@@ -86,8 +34,6 @@ class CPS2Format(FileFormat.FileFormat):
             even = interleave(sdata[0], next_data[0], 2)
             odd = interleave(sdata[1], next_data[1], 2)
             interleaved.append((even, odd))
-        # first_interleave = [interleave(pair, 2) for pair in reshaped]
-        # reshaped = [first_interleave[i:i+2] for i in range(0, len(first_interleave), 2)]
 
         self.verboseprint('Second pass - interleaving every 64 bytes')
         inter_iter = iter(interleaved)
@@ -97,11 +43,9 @@ class CPS2Format(FileFormat.FileFormat):
             next_data = next(inter_iter)
             second_interleave.append(interleave(i[0], next_data[0], 64))
             second_interleave.append(interleave(i[1], next_data[1], 64))
-        # second_interleave = [interleave(pair, 64) for pair in reshaped]
 
         self.verboseprint('Last pass - interleaving every 1048576 bytes')
         final = [interleave(second_interleave[0], second_interleave[1], 1048576)]
-        # final = [interleave(second_interleave, 1048576)]
 
         return final
 
@@ -109,7 +53,7 @@ class CPS2Format(FileFormat.FileFormat):
         """Deinterleaves a interleaved cps2 graphics file."""
 
         self.verboseprint('Opening files')
-        data = file_manip.open_file(self._filepaths[0])
+        data = self.open_file(self._filepaths[0])
 
         self.verboseprint('First pass - deinterleaving every 1048576 bytes')
         first = deinterleave(data, 1048576)
@@ -128,10 +72,9 @@ class CPS2Format(FileFormat.FileFormat):
 
         return deinterleaved
 
-    def format_filenames(self):
+    def _filenames_and_suffixes(self):
         if len(self._filepaths) == 1:
-            # print("deinterleavE")
-            #assemble parts for saving the file
+            # print("deinterleave")
             filepaths = [os.path.split(fpath)[1] for fpath in self._filepaths]
             splits = filepaths[0].split('.')
             suffixes = splits[1:-1]
@@ -145,6 +88,11 @@ class CPS2Format(FileFormat.FileFormat):
             nums = [str(num[1]) for num in splits]
             filenames = ['.'.join([bases[0], *nums])]
             suffixes = ['combined']
+
+        return filenames, suffixes
+
+    def format_savepaths(self):
+        filenames, suffixes = self._filenames_and_suffixes()
 
         #if no custom output, save to cwd with default name
         if not self._savepaths:
@@ -161,15 +109,41 @@ class CPS2Format(FileFormat.FileFormat):
         else:
             spaths = ['.'.join([self._savepaths, s]) for s in suffixes]
 
-        # print('spaths is:', spaths)
         return spaths
 
-    def save(self, savepaths, savedata):
-        for spath, data in zip(savepaths, savedata):
-            with open(spath, 'wb') as f:
-                self.verboseprint('Saving', spath)
-                f.write(data)
+# factory
+def new(filepaths, savepaths, verbose):
+    return CPS2Format(filepaths, None, savepaths, verbose)
 
-#can remove 'numbytes' from function header
-def new(filepaths, numbytes, savepaths, verbose):
-    return CPS2Format(filepaths, numbytes, savepaths, verbose)
+def deinterleave(data, num_bytes):
+    """Deinterleaves a bytearray.
+
+    Returns two bytearrays.
+    """
+    evens = []
+    odds = []
+    deinterleave_s = Struct('c' * num_bytes)
+    deinterleave_iter = deinterleave_s.iter_unpack(data)
+
+    for i in deinterleave_iter:
+        evens.extend([*i])
+        odds.extend([*next(deinterleave_iter)])
+
+    return b''.join(evens), b''.join(odds)
+
+def interleave(file1, file2, num_bytes):
+    """Interleaves two bytearray buffers together.
+
+    Returns a bytearray.
+    """
+    interleaved = []
+    interleave_s = Struct('c' * num_bytes)
+    file1_iter = interleave_s.iter_unpack(file1)
+    file2_iter = interleave_s.iter_unpack(file2)
+
+    for i in file1_iter:
+        file2_next = next(file2_iter)
+        interleave_temp = [*i, *file2_next]
+        interleaved.extend(interleave_temp)
+
+    return  b''.join(interleaved)
--- file_manip_toolkit/unfman/CustomFormat.py
+++ file_manip_toolkit/unfman/CustomFormat.py
@@ -1,55 +1,61 @@
 import os
+import sys
+from struct import Struct, error
 from file_manip_toolkit.helpers import is_number
-from file_manip_toolkit.unfman import file_manip, FileFormat
+from file_manip_toolkit.unfman.FileFormat import FileFormat
 
-#Other possible names, GenericFormat / NoFormat ?
-class CustomFormat(FileFormat.FileFormat):
+class CustomFormat(FileFormat):
     def run(self):
-        if len(self._filepaths) == 2:
-            if is_number(self._filepaths[1]):
-                self.nsplit = int(self._filepaths[1])
+        if is_number(self._filepaths[1]):
+            self._nsplit = int(self._filepaths[1])
 
-                final = self.deinterleave_file()
+        if self._nsplit:
+            final = self.deinterleave_file()
 
-                filenames = [os.path.split(self._filepaths[0])[1]] * self._nsplit
-                suffixes = [str(i) for i in range(self._nsplit)]
-
-            else:
-                final = self.interleave_files()
-
-                filenames = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
-                suffixes = ['combined']
-
-        elif len(self._filepaths) > 2:
+        elif len(self._filepaths) >= 2:
             final = self.interleave_files()
-
-            filenames = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
-            suffixes = ['combined']
 
         else:
             print('Something broke - number of filepaths:', len(self._filepaths))
             raise Exception
 
-        self.save(final, filenames, suffixes)
+        savepaths = self.format_savepaths()
+        self.save(savepaths, final)
 
     def interleave_files(self):
         self.verboseprint('Opening files')
-        data = [file_manip.open_file(fp) for fp in self._filepaths]
+        data = [self.open_file(fp) for fp in self._filepaths]
 
         self.verboseprint('Interleaving files every', self._numbytes, 'bytes')
 
-        return [file_manip.interleave(data, int(self._numbytes))]
+        return [interleave(data, int(self._numbytes))]
 
     def deinterleave_file(self):
         self.verboseprint('Opening file')
-        data = file_manip.open_file(self._filepaths[0])
+        data = self.open_file(self._filepaths[0])
 
         self.verboseprint('Deinterleaving file every', self._numbytes, 'bytes')
         self.verboseprint('Producing', self._nsplit, 'files')
 
-        return file_manip.deinterleave(data, int(self._numbytes), self._nsplit)
+        return deinterleave(data, int(self._numbytes), self._nsplit)
 
-    def save(self, savedata, filenames, suffixes):
+    def _filenames_and_suffixes(self):
+        if self._nsplit:
+            filenames = [os.path.split(self._filepaths[0])[1]] * self._nsplit
+            suffixes = [str(i) for i in range(self._nsplit)]
+
+        # elif len(self._filepaths) > 2:
+        #     filenames = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
+        #     suffixes = ['combined']
+
+        else:
+            filenames = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
+            suffixes = ['combined']
+
+        return filenames, suffixes
+
+    def format_savepaths(self):
+        filenames, suffixes = self._filenames_and_suffixes()
         #if no custom output, save to cwd with default name
         if not self._savepaths:
             fnames = [os.path.split(fname)[1] for fname in filenames]
@@ -58,20 +64,67 @@ class CustomFormat(FileFormat.FileFormat):
         #if custom output is a folder, save default file name to that location
         elif os.path.isdir(self._savepaths):
             head = self._savepaths
-            # print('filenames is:', filenames)
             tails = ['.'.join([fname, s]) for fname, s in zip(filenames, suffixes)]
-            # print('tails is:', tails)
             spaths = [os.path.join(head, tail) for tail in tails]
 
         #if custom output is a file, append number to the end of it
         else:
             spaths = ['.'.join([self._savepaths, s]) for s in suffixes]
 
-        for savepath, data in zip(spaths, savedata):
-            with open(savepath, 'wb') as f:
-                self.verboseprint('Saving', savepath)
-                f.write(data)
+        return spaths
 
 # Factory method
 def new(filepaths, numbytes, savepaths, verbose):
     return CustomFormat(filepaths, numbytes, savepaths, verbose)
+
+def deinterleave(data, nbytes, nsplit):
+    """Deinterleaves one bytearray into nsplit many bytearrays on a nbytes basis.
+
+    Returns a list of bytearrays.
+    """
+    deinterleaved = [[] for n in range(nsplit)]
+
+    deinterleave_s = Struct('c' * nbytes)
+
+    try:
+        deinterleave_iter = deinterleave_s.iter_unpack(data)
+    except error as err:
+        #this error can be many things, handling generically until otherwise
+        print('ERROR:', err, 'CLOSING', file=sys.stderr)
+        raise err
+
+    #this could cause rounding errors?
+    iterlen = int(len(data) / (nbytes * nsplit))
+    for _ in range(iterlen):
+        for i, _ in enumerate(deinterleaved):
+            try:
+                next_ = next(deinterleave_iter)
+            except StopIteration:
+                pass
+            deinterleaved[i].extend([*next_])
+
+    return [b''.join(delist) for delist in deinterleaved]
+
+def interleave(data, nbytes):
+    """Interleaves a list of bytearrays together on a nbytes basis.
+
+    Returns a bytearray.
+    """
+    interleave_s = Struct('c' * nbytes)
+    iters = []
+
+    for inter in data:
+        try:
+            iters.append(interleave_s.iter_unpack(inter))
+        except error as err:
+            print('ERROR:', err, 'CLOSING', file=sys.stderr)
+            raise err
+
+    interleaved = []
+    #this could cause rounding errors?
+    iterlen = int(len(data[0]) / nbytes)
+    for _ in range(iterlen):
+        nexts = [next(iter_) for iter_ in iters]
+        interleaved.extend([b''.join(val) for val in nexts])
+
+    return b''.join(interleaved)
