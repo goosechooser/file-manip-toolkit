8ca18a803212543696c29851b7c17cbaef977d15
Marshall <xtremebass2k@gmail.com>
2017-02-24 01:10:20 -0800

    fixed skipped tests

--- tests/file_manip_test.py
+++ tests/file_manip_test.py
@@ -1,6 +1,6 @@
 import struct
 import pytest
-from file_manip_toolkit.unfman.CustomFormat import interleave, deinterleave
+from file_manip_toolkit.unfman.CustomFormat import CustomFormat, interleave, deinterleave
 
 # This is probably gonna get deleted/moved to other places
 
@@ -9,16 +9,14 @@ TESTDATA2 = bytearray.fromhex('FA FB FC FD AF BF CF DF')
 
 TESTFILE = 'testdir\\vm3.15'
 
-def test_deinterleave():
-    result = deinterleave(TESTDATA, 1, 2)
-    assert result[0] == bytearray.fromhex('0A 0C A0 C0')
-    assert result[1] == bytearray.fromhex('0B 0D B0 D0')
-
-    result = deinterleave(TESTDATA, 2, 2)
-    assert result[0] == bytearray.fromhex('0A 0B A0 B0')
-    assert result[1] == bytearray.fromhex('0C 0D C0 D0')
-
-    #more tests related nbytes/nsplit?
+@pytest.mark.parametrize('test_input, nsplit, expected', [
+    (TESTDATA, 1, ('0A 0C A0 C0', '0B 0D B0 D0')),
+    (TESTDATA, 2, ('0A 0B A0 B0', '0C 0D C0 D0')),
+])
+def test_deinterleave(test_input, nsplit, expected):
+    result = deinterleave(test_input, nsplit, 2)
+    assert result[0] == bytearray.fromhex(expected[0])
+    assert result[1] == bytearray.fromhex(expected[1])
 
 #todo - figure out what exception the following cases are raising
 #(TESTDATA, 8, 2),
@@ -26,8 +24,6 @@ def test_deinterleave():
 @pytest.mark.parametrize('test_input, nbytes, nsplit', [
     (TESTDATA, 3, 2),
     (TESTDATA, 99, 2),
-    #(TESTDATA, 8, 2),
-    #(TESTDATA, 1, 1),
 ])
 def test_deinterleave_exception(test_input, nbytes, nsplit):
     with pytest.raises(struct.error):
@@ -45,16 +41,14 @@ def test_interleave_exception():
     with pytest.raises(struct.error):
         interleave([TESTDATA, TESTDATA2], 99)
 
-@pytest.mark.skip
 def test_open_file():
-    assert open_file(TESTFILE)
+    assert CustomFormat.open_file(TESTFILE)
 
-@pytest.mark.skip
 def test_open_file_exception(tmpdir):
     fn = tmpdir.mkdir('data')
 
     with pytest.raises(OSError):
-        open_file(str(fn))
+        CustomFormat.open_file(str(fn))
 
     with pytest.raises(FileNotFoundError):
-        open_file("vm.txt")
+        CustomFormat.open_file("vm.txt")
